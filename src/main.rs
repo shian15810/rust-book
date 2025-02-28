bdd14f927b828e287e415b9016c8e863a5c924f6
Cheeshian Chuah <shian15810@gmail.com>
2025-02-28 17:25:44 +0000

    Learn hello_cargo of chapter 1.3

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 // Chapter 1
+pub mod hello_world;
 
-mod hello_world;
+mod hello_cargo;
 
 fn main() {
     // println!("Hello, world!");
 
-    hello_world::main(); // Chapter 1.2
+    // hello_world::main(); // Chapter 1.2
+
+    hello_cargo::main(); // Chapter 1.3
 }
