a850cd0f14aebf7ca3ca46dec201b30e52149e93
Cheeshian Chuah <shian15810@gmail.com>
2025-02-28 17:36:29 +0000

    Learn guessing_game of chapter 2

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 // Chapter 1
+pub mod hello_cargo;
 pub mod hello_world;
 
-mod hello_cargo;
+// Chapter 2
+
+mod guessing_game;
 
 fn main() {
     // println!("Hello, world!");
 
     // hello_world::main(); // Chapter 1.2
+    // hello_cargo::main(); // Chapter 1.3
 
-    hello_cargo::main(); // Chapter 1.3
+    guessing_game::main(); // Chapter 2
 }
