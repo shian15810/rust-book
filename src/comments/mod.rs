e61dea2404554ecffc94531bb2a1febe3bcbbdcf
Cheeshian Chuah <shian15810@gmail.com>
2025-03-01 16:27:21 +0000

    Learn comments of chapter 3.4

---
+++ src/comments/mod.rs
@@ -0,0 +1,13 @@
+#[allow(unused_variables)]
+pub fn main() {
+    // hello, world
+
+    // So we’re doing something complicated here, long enough that we need
+    // multiple lines of comments to do it! Whew! Hopefully, this comment will
+    // explain what’s going on.
+
+    let lucky_number = 7; // I'm feeling lucky today
+
+    // I'm feeling lucky today
+    let lucky_number = 7;
+}
--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,11 @@ pub mod hello_world;
 pub mod guessing_game;
 
 // Chapter 3
+pub mod functions;
 pub mod no_type_annotations;
 pub mod variables;
 
-mod functions;
+mod comments;
 
 fn main() {
     // println!("Hello, world!");
@@ -21,6 +22,7 @@ fn main() {
 
     // variables::main();           // Chapter 3.1
     // no_type_annotations::main(); // Chapter 3.2
+    // functions::main();           // Chapter 3.3
 
-    functions::main(); // Chapter 3.3
+    comments::main(); // Chapter 3.4
 }
