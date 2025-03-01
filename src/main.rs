f469a5bdce05372ba70ebf606168664d991a0b9d
Cheeshian Chuah <shian15810@gmail.com>
2025-03-01 06:32:01 +0000

    Learn variables of chapter 3.1

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,11 @@ pub mod hello_cargo;
 pub mod hello_world;
 
 // Chapter 2
+pub mod guessing_game;
 
-mod guessing_game;
+// Chapter 3
+
+mod variables;
 
 fn main() {
     // println!("Hello, world!");
@@ -12,5 +15,7 @@ fn main() {
     // hello_world::main(); // Chapter 1.2
     // hello_cargo::main(); // Chapter 1.3
 
-    guessing_game::main(); // Chapter 2
+    // guessing_game::main();   // Chapter 2
+
+    variables::main(); // Chapter 3.1
 }
